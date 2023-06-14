c00898a03eac1973e0f5b980d92baf3bdb733a10
franckp1 <74624674+franckp1@users.noreply.github.com>
2023-06-14 13:04:33 -0500

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="PYpirepfepc",
-    version="0.1",
+    version="0.2",
     license="MIT",
     description="Cuestionario para reforzar tus conocimientos de python estilo juego trivia",
     author="Franck Pérez C",
